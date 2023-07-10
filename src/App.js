ae80fb922650fbad2c133f45408f2c5e8baeeb6a
daro-asecas <daro.1919@gmail.com>
2023-07-10 12:42:16 -0300

    VersionDefinitiva

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { evaluate } from "mathjs"
+import Popup from 'reactjs-popup';
 
 import { WrittingBox } from "./components/WrittingBox"
 import { Result } from "./components/Result"
@@ -10,6 +11,7 @@ import './style.css';
 
 export default function App() {
 
+    const [charactersArray, setCharactersArray] = useState([])
     const [writtingSlot, setWrittingSlot] = useState("")
     const [resultMemory, setResultMemory] = useState("0")
     const [resultDisplay, setResultDisplay] = useState("0")
@@ -17,14 +19,13 @@ export default function App() {
     const [justEvaluated, setJustEvaluated] = useState(false)
 
     function write(character) {
-//        if (writtingSlot==0 && character==0) return
-//        if (writtingSlot==0 && (type==="number" || type==="constant")) {
-//            setWrittingSlot(`${character}`);
-//                return
-//        } else {
-//            setWrittingSlot(`${writtingSlot}${character}`);
-//        }
         setWrittingSlot(`${writtingSlot}${character}`);
+        setCharactersArray([...charactersArray,character.length])
+
+        console.log(character)
+        console.log(character.length)
+        console.log(charactersArray)
+
         setJustEvaluated(false);
     }
 
@@ -39,7 +40,9 @@ export default function App() {
 
     function deleteLast() {
         if (writtingSlot.length === 0) return
-        setWrittingSlot(writtingSlot.slice(0, -1));
+
+        const lengtToDelete = charactersArray.pop()
+        setWrittingSlot(writtingSlot.slice(0, -lengtToDelete));
     }
 
     function clearWrittingSlot() {
@@ -96,74 +99,98 @@ export default function App() {
 */}
 
   return (
-    <table>
-        <thead>
-            <WrittingBox display={writtingSlot} />
-            <Result display={resultDisplay} />
-        </thead>
+    <>
 
-        <tbody>
-            <tr>
-                <Button display="MC" color="black" callback={memoryClear} />
-                <Button display="MS" color="black" callback={memorySave} />
-                <Button display="M" color="black" callback={memoryWrite} />
+        <table>
+            <thead>
+                <WrittingBox display={writtingSlot} />
+                <Result display={resultDisplay} />
+            </thead>
 
-                <Button display="DEL" color="red" callback={deleteLast} />
-                <Button display="AC"  color="red" callback={clearWrittingSlot} />
-            </tr>
+            <tbody>
+                <tr>
+                    <Button display="MC" color="black" callback={memoryClear} />
+                    <Button display="MS" color="black" callback={memorySave} />
+                    <Button display="M" color="black" callback={memoryWrite} />
 
-            <tr>
-                <Button display="sin" write="sin(" color="gray" callback={write} />
-                <Button display="cos" write="cos(" color="gray" callback={write} />
-                <Button display="tan" write="tan(" color="gray" callback={write} />
-                <Button display="º" write="deg" color="gray" callback={write} />
-                <Button display="π" write="PI" color="gray" callback={write} />
-            </tr>
+                    <Button display="DEL" color="red" callback={deleteLast} />
+                    <Button display="AC"  color="red" callback={clearWrittingSlot} />
+                </tr>
 
-            <tr>
-                <Button display="!" color="gray" callback={write} />
-                <Button display="√" write="sqrt(" color="gray" callback={write} />
-                <Button display="log" write="log(" color="gray" callback={write} />
-                <Button display="ln" write="ln(" color="gray" callback={write} />
-                <Button display="e" color="gray" callback={write} />
-            </tr>
+                <tr>
+                    <Button display="sin" write="sin(" color="gray" callback={write} />
+                    <Button display="cos" write="cos(" color="gray" callback={write} />
+                    <Button display="tan" write="tan(" color="gray" callback={write} />
+                    <Button display="º" write="deg" color="gray" callback={write} />
+                    <Button display="π" write="PI" color="gray" callback={write} />
+                </tr>
 
-            <tr>
-                <Button display="7" color="blue" callback={write} />
-                <Button display="8" color="blue" callback={write} />
-                <Button display="9" color="blue" callback={write} />
+                <tr>
+                    <Button display="!" color="gray" callback={write} />
+                    <Button display="√" write="sqrt(" color="gray" callback={write} />
+                    <Button display="log" write="log(" color="gray" callback={write} />
+                    <Button display="ln" write="ln(" color="gray" callback={write} />
+                    <Button display="e" color="gray" callback={write} />
+                </tr>
 
-                <Button display="(" color="green" callback={write} />
-                <Button display=")" color="green" callback={write} />
-            </tr>
+                <tr>
+                    <Button display="7" color="blue" callback={write} />
+                    <Button display="8" color="blue" callback={write} />
+                    <Button display="9" color="blue" callback={write} />
 
-            <tr>
-                <Button display="4" color="blue" callback={write} />
-                <Button display="5" color="blue" callback={write} />
-                <Button display="6" color="blue" callback={write} />
+                    <Button display="(" color="green" callback={write} />
+                    <Button display=")" color="green" callback={write} />
+                </tr>
 
-                <Button display="X" color="green" callback={operateOnAns} />
-                <Button display="/" color="green" callback={operateOnAns} />
-            </tr>
+                <tr>
+                    <Button display="4" color="blue" callback={write} />
+                    <Button display="5" color="blue" callback={write} />
+                    <Button display="6" color="blue" callback={write} />
 
-            <tr>
-                <Button display="1" color="blue" callback={write} />
-                <Button display="2" color="blue" callback={write} />
-                <Button display="3" color="blue" callback={write} />
+                    <Button display="X" color="green" callback={operateOnAns} />
+                    <Button display="/" color="green" callback={operateOnAns} />
+                </tr>
 
-                <Button display="+" color="green" callback={operateOnAns} />
-                <Button display="-" color="green" callback={operateOnAns} />
-            </tr>
+                <tr>
+                    <Button display="1" color="blue" callback={write} />
+                    <Button display="2" color="blue" callback={write} />
+                    <Button display="3" color="blue" callback={write} />
 
-            <tr>
-                <Button display="0" color="blue" callback={write} />
-                <Button display="." color="blue" callback={write} />
-                <Button display="Ans" color="blue" callback={write} />
+                    <Button display="+" color="green" callback={operateOnAns} />
+                    <Button display="-" color="green" callback={operateOnAns} />
+                </tr>
 
-                <Button display="^" color="green" callback={operateOnAns} />
-                <Button display="=" color="green" callback={evaluateInput} />
-            </tr>
-        </tbody>
-    </table>
+                <tr>
+                    <Button display="0" color="blue" callback={write} />
+                    <Button display="." color="blue" callback={write} />
+                    <Button display="Ans" color="blue" callback={write} />
+
+                    <Button display="^" color="green" callback={operateOnAns} />
+                    <Button display="=" color="green" callback={evaluateInput} />
+                </tr>
+
+                <tr>
+                    <td colSpan={"100%"} id="popup-button" className="button">
+                        <Popup trigger= {<button> Información del Proyecto </button>} modal nested>
+                            {close => (
+                                    <div id="popup-content" className='modal'>
+                                        <div className='content'>
+                                            <p>Esta es mi implementación de calculadora cientifica con REACT.</p>
+                                            <p>Mi idea fue agregar la funcionalidad de una calculadora real, manteniendo al máximo la estética simple del proyecto original.</p>
+                                            <p>Lo unico que agregué fue una linea para mostrar el resultado diferente a la linea de escritura de funciones.</p>
+                                        </div>
+                                        <div id="popup-close-button" className="button">
+                                            <button onClick={() => close()}>Cerrar</button>
+                                        </div>
+                                    </div>
+                                )}
+                        </Popup>
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+
+    </>
   );
 }
